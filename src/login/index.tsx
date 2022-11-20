c5608e6f0374b6705aa434cec9b80bb5bc5be1b5
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-20 17:39:50 -0500

    Remove user icon when email field is no longer empty

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -36,7 +36,7 @@ function LoginPage() {
             <div className={styles.formContainer}>
                 <div className={styles.inputContainer}>
                     <input placeholder={'Email'} type={'email'} className={styles.input} value={email} onChange={(event) => setEmail(event.target.value)}/>
-                    <UserIcon/>
+                    <UserIcon field={email}/>
                 </div>
                 <div className={styles.inputContainer}>
                     <input placeholder={'Password'} type={'password'} className={styles.input} value={password} onChange={(event) => setPassword(event.target.value)}/>
@@ -54,12 +54,17 @@ function LoginPage() {
     );
 }
 
-function UserIcon() {
+function UserIcon(props: any) {
     return (
-        <svg width="40" height="40" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.inputIcon}>
-            <path d="M28 29V27C28 25.9391 27.5786 24.9217 26.8284 24.1716C26.0783 23.4214 25.0609 23 24 23H16C14.9391 23 13.9217 23.4214 13.1716 24.1716C12.4214 24.9217 12 25.9391 12 27V29" stroke="#ADADAD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M20 19C22.2091 19 24 17.2091 24 15C24 12.7909 22.2091 11 20 11C17.7909 11 16 12.7909 16 15C16 17.2091 17.7909 19 20 19Z" stroke="#ADADAD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+        <>
+            {
+                (!props.field || props.field.length === 0) &&
+                <svg width="40" height="40" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.inputIcon}>
+                    <path d="M28 29V27C28 25.9391 27.5786 24.9217 26.8284 24.1716C26.0783 23.4214 25.0609 23 24 23H16C14.9391 23 13.9217 23.4214 13.1716 24.1716C12.4214 24.9217 12 25.9391 12 27V29" stroke="#ADADAD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M20 19C22.2091 19 24 17.2091 24 15C24 12.7909 22.2091 11 20 11C17.7909 11 16 12.7909 16 15C16 17.2091 17.7909 19 20 19Z" stroke="#ADADAD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            }
+        </>
     );
 }
 
