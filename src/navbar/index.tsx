2c789dd6f52bce2f173b34683d12bbba2e04b787
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-20 15:32:40 -0500

    Add waves and dots

--- src/navbar/index.tsx
+++ src/navbar/index.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import styles from './styles.module.css';
+import styles from './navstyles.module.css';
 import logo from '../logo.png';
 
 function Navbar() {
     return (
         <div className={styles.navbarContainer}>
             <div className={styles.navbarInnerContainer}>
-                <img src={logo} alt={'The Secret Cocktail company logo'} className={styles.navbarLogo}/>
-                <div className={styles.navbarTitleText}>The Secret Cocktail</div>
-                <div className={styles.navbarBtnsContainer}>
-                    <button className={styles.loginBtn} onClick={() => {alert('login')}}>Login</button>
-                    <button className={styles.signUpBtn} onClick={() => {alert('sign-up')}}>Sign Up</button>
-                </div>
+                <Logo/>
+                <TitleText/>
+                <AuthButtons/>
             </div>
+        </div>
+    );
+}
+
+function Logo() {
+    return (
+        <img src={logo} alt={'The Secret Cocktail company logo'} className={styles.navbarLogo}/>
+    );
+}
+
+function TitleText() {
+    return (
+        <div className={styles.navbarTitleText}>The Secret Cocktail</div>
+    );
+}
+
+function AuthButtons() {
+    return (
+        <div className={styles.navbarBtnsContainer}>
+            <button className={styles.loginBtn} onClick={() => {alert('login')}}>Login</button>
+            <button className={styles.signUpBtn} onClick={() => {alert('sign-up')}}>Sign Up</button>
         </div>
     );
 }
