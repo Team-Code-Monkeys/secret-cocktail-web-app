e8c34224449fb207a33d2451602d77de8f1a0181
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-20 20:07:18 -0500

    Detection of slow Wi-Fi to prevent user confusion on initial blank page

--- src/authredirect/index.tsx
+++ src/authredirect/index.tsx
@@ -4,6 +4,11 @@ import {getAuth} from 'firebase/auth';
 import {useNavigate} from "react-router-dom";
 import {setupAuthListener} from "./setup-auth-listener";
 
+/**
+ * Page that shows blank content until auth state is finalized.
+ * If the user is logged in, go to their home page. Otherwise, go to the landing page.
+ * Note: for slow Wi-Fi this will show a blank page for a while, so I added a 3 second timeout.
+ */
 function AuthRedirectPage() {
     const auth = getAuth(firebaseApp);
     const navigate = useNavigate();
@@ -11,6 +16,16 @@ function AuthRedirectPage() {
     useEffect(() => {
         setupAuthListener(auth, navigate);
     }, [auth, navigate]);
+
+    useEffect(() => {
+        const slowWiFiLimitSeconds = 3;
+        const timeoutID = setTimeout(() => {
+            alert('Slow Wi-Fi detected')
+        }, slowWiFiLimitSeconds * 1000.0);
+        return () => {
+            clearTimeout(timeoutID);
+        };
+    }, [])
 
     return (
         <div/>
--- src/authredirect/setup-auth-listener.tsx
+++ src/authredirect/setup-auth-listener.tsx
@@ -3,10 +3,14 @@ import {k_landing_page_route, k_map_page_route} from "../index";
 import {Auth} from "@firebase/auth";
 import {NavigateFunction} from "react-router/dist/lib/hooks";
 
+/**
+ * Automatically redirect the user to their respective homepage if they are already logged in, otherwise go to landing page
+ * @param auth Firebase auth state of user
+ * @param navigate React Router function to navigate the user to a different page
+ */
 export const setupAuthListener = (auth: Auth, navigate: NavigateFunction) => {
     if (auth) {
         auth.onAuthStateChanged((user: any) => {
-            console.log(user);
             if (user) {
                 // TODO: check user role (trainee, facility, or admin) to determine which page to navigate to
                 navigate(k_map_page_route);
