e85c70146da575afe7c898e84d3e9f7d419529e7
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-21 11:34:01 -0500

    Display distance to facilities

--- src/map/index.tsx
+++ src/map/index.tsx
@@ -21,7 +21,7 @@ function MapPage() {
     const navigate = useNavigate();
     const [center, setCenter] = useState<any>([33.78010647946605, -84.38955018824828]);
     const [zoom, setZoom] = useState<number>(16.0);
-    const [radius, setRadius] = useState<number>(300);
+    const [radius, setRadius] = useState<number>(444);
     const [facilities, setFacilities] = useState<any>([])
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     // const [facilities, setFacilities] = useState([{
@@ -59,8 +59,10 @@ function MapPage() {
                         const lat = doc?.data()?.geopoint?.latitude;
                         const lon = doc?.data()?.geopoint?.longitude;
                         if (lat && lon) {
-                            // geo hash not 100% accurate, do double-check distance
-                            if (distanceBetween([lat, lon] as Geopoint, c) <= radiusInM) {
+                            // geo hash not 100% accurate, have to double-check distance
+                            const distance = distanceBetween([lat, lon] as Geopoint, c) * 1000;
+                            console.log(distance);
+                            if (distance <= radiusInM) {
                                 const newFacility: any = doc.data();
                                 newFacility.id = doc.id;
                                 newFacilities.push(newFacility);
@@ -79,7 +81,7 @@ function MapPage() {
             <Navbar/>
             {/*TODO: allow admin user to delete facility*/}
             <div className={styles.innerContainer}>
-                <FacilityList facilities={facilities}/>
+                <FacilityList facilities={facilities} center={center}/>
                 <div className={styles.mapView}>
                     <Wrapper apiKey={GOOGLE_MAPS_API_KEY} render={render}>
                         <MapComponent center={center} setCenter={setCenter} zoom={zoom} setZoom={setZoom}>
@@ -92,19 +94,27 @@ function MapPage() {
                                                 lat: facility?.geopoint?.latitude || 0.0,
                                                 lng: facility?.geopoint?.longitude || 0.0,
                                             }}
-                                            label={facility?.name || 'No name'}
+                                            label={{color: 'black', text: facility?.name || 'No name'}}
                                         />
                                     );
                                 })
                             }
+                            <Marker
+                                key={'user'}
+                                position={{
+                                    lat: center[0],
+                                    lng: center[1],
+                                }}
+                                label={{color: 'black', text: 'You'}}
+                            />
                             <Circle
                                 center={{
                                     lat: center[0],
                                     lng: center[1],
                                 }}
                                 radius={radius}
-                                fillColor={'grey'}
-                                strokeColor={'grey'}
+                                fillColor={'rgba(237,199,255,0.45)'}
+                                strokeColor={'rgba(153,79,169,0.71)'}
                             />
                         </MapComponent>
                     </Wrapper>
@@ -115,6 +125,10 @@ function MapPage() {
 }
 
 function FacilityList(props: any) {
+    function getMiles(meters: number): number {
+        return meters * 0.000621371192;
+    }
+
     return (
         <div className={styles.listView}>
             <div>Your Location</div>
@@ -122,12 +136,14 @@ function FacilityList(props: any) {
                 console.log(event.target.value)
             }}/>
             {props.facilities.map((facility: any, index: number) => {
+                const distanceInMeters = Math.round(distanceBetween(props?.center || [0, 0], [facility?.geopoint?.latitude || 0.0, facility?.geopoint?.longitude || 0.0]) * 1000.0);
+                const distance = getMiles(distanceInMeters);
                 return (
                     <div key={facility.id}>
                         <div>{`NAME: ${facility.name}`}</div>
                         <div>{`ADDRESS: ${facility.address}`}</div>
                         <div>{`PHONE: ${facility.phone}`}</div>
-                        <div>{`DISTANCE: ${facility.distance}`}</div>
+                        <div>{`DISTANCE: ${distance} miles`}</div>
                     </div>
                 );
             })}
