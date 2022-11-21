dd1cd3bc07fa9e40562fe81335f3bc6c20490fd1
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-21 01:13:44 -0500

    Add markers

--- src/map/index.tsx
+++ src/map/index.tsx
@@ -7,6 +7,7 @@ import {setupAuthListener} from '../authredirect/setup-auth-listener';
 import firebaseApp from '../firebase';
 import {checkedIfAllowedOnPage, k_admin_role, k_regular_user_role} from "../authredirect/auth-check";
 import {Wrapper, Status} from "@googlemaps/react-wrapper";
+import {GOOGLE_MAPS_API_KEY} from "../api";
 
 const render = (status: Status) => {
     return <h1>{status}</h1>;
@@ -38,25 +39,12 @@ function MapPage() {
             <Navbar/>
             {/*TODO: allow admin user to delete facility*/}
             <div className={styles.innerContainer}>
-                <div className={styles.listView}>
-                    <div>Your Location</div>
-                    <input value={'930 Spring Street NW'} onChange={(event) => {
-                        console.log(event.target.value)
-                    }}/>
-                    {facilities.map(function (facility, index) {
-                        return (
-                            <div key={facility.id}>
-                                <div>{`NAME: ${facility.name}`}</div>
-                                <div>{`ADDRESS: ${facility.address}`}</div>
-                                <div>{`PHONE: ${facility.phone}`}</div>
-                                <div>{`DISTANCE: ${facility.distance}`}</div>
-                            </div>
-                        );
-                    })}
-                </div>
+                <FacilityList facilities={facilities}/>
                 <div className={styles.mapView}>
-                    <Wrapper apiKey={'AIzaSyCpHG9-94DvB3FDFYLX8weS0QgyxNDESiQ'} render={render}>
-                        <MapComponent/>
+                    <Wrapper apiKey={GOOGLE_MAPS_API_KEY} render={render}>
+                        <MapComponent>
+                            <Marker position={{lat: 33.78010647946605, lng: -84.38955018824828}} />
+                        </MapComponent>
                     </Wrapper>
                 </div>
             </div>
@@ -64,7 +52,32 @@ function MapPage() {
     );
 }
 
-function MapComponent({...props}) {
+function FacilityList(props: any) {
+    return (
+        <div className={styles.listView}>
+            <div>Your Location</div>
+            <input value={'930 Spring Street NW'} onChange={(event) => {
+                console.log(event.target.value)
+            }}/>
+            {props.facilities.map((facility: any, index: number) => {
+                return (
+                    <div key={facility.id}>
+                        <div>{`NAME: ${facility.name}`}</div>
+                        <div>{`ADDRESS: ${facility.address}`}</div>
+                        <div>{`PHONE: ${facility.phone}`}</div>
+                        <div>{`DISTANCE: ${facility.distance}`}</div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+interface MapComponentProps {
+    children: any
+}
+
+function MapComponent(props: MapComponentProps) {
     const ref = React.useRef<HTMLDivElement>(null);
     const [map, setMap] = React.useState<google.maps.Map>();
 
@@ -78,8 +91,44 @@ function MapComponent({...props}) {
     }, [ref, map]);
 
     return (
-        <div ref={ref} style={{width: '100%', height: '100%'}} {...props}/>
+        // <div ref={ref} style={{width: '100%', height: '100%'}} {...props}/>
+        <>
+            <div ref={ref} style={{width: '100%', height: '100%'}} />
+            {React.Children.map(props.children, (child) => {
+                if (React.isValidElement(child)) {
+                    // set the map prop on the child component
+                    // @ts-ignore
+                    return React.cloneElement(child, { map });
+                }
+            })}
+        </>
     );
 }
+
+const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
+    const [marker, setMarker] = React.useState<google.maps.Marker>();
+
+    React.useEffect(() => {
+        if (!marker) {
+            setMarker(new google.maps.Marker());
+        }
+
+        // remove marker from map on unmount
+        return () => {
+            if (marker) {
+                marker.setMap(null);
+            }
+        };
+    }, [marker]);
+
+    React.useEffect(() => {
+        if (marker) {
+            marker.setOptions(options);
+        }
+    }, [marker, options]);
+
+    return null;
+};
+
 
 export default MapPage;
