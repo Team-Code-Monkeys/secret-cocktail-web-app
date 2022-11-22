5ad1278a9b1d9b1caf3e9d6ce4adc6cc152dc1a7
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-21 22:34:28 -0500

    Create blank phone survey page

--- src/phonesurvey/index.tsx
+++ src/phonesurvey/index.tsx
@@ -7,6 +7,7 @@ import {getAuth} from "firebase/auth";
 import {useNavigate} from "react-router-dom";
 import firebaseApp from "../firebase";
 import {checkedIfAllowedOnPage, k_admin_role} from "../authredirect/auth-check";
+import {k_admin_portal_page_route} from "../index";
 
 function AdminPhoneSurveyPage() {
     const auth = getAuth(firebaseApp);
@@ -20,8 +21,24 @@ function AdminPhoneSurveyPage() {
     return (
         <div className={styles.container}>
             <Navbar/>
-            <div>
-                <div>hello world</div>
+            <div className={styles.innerContainer2}>
+                <div className={styles.title}>Phone Survey Dashboard</div>
+            </div>
+            <div className={styles.innerContainer3}>
+
+            </div>
+            <div className={styles.innerContainer}>
+                <div className={styles.backBtnContainer} onClick={() => {
+                    navigate(k_admin_portal_page_route)
+                }}>
+                    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M27 20H13" stroke="#5C5C5C" strokeWidth="2" strokeLinecap="round"
+                              strokeLinejoin="round"/>
+                        <path d="M20 27L13 20L20 13" stroke="#5C5C5C" strokeWidth="2" strokeLinecap="round"
+                              strokeLinejoin="round"/>
+                    </svg>
+                    <div className={styles.backBtnText}>Back</div>
+                </div>
             </div>
             <Waves/>
         </div>
