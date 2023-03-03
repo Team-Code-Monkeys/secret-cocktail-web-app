7e9bbf0f189250e1a8ace524fc6c88d36e28ed35
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-03 01:29:37 -0500

    Converting existing server code to Firebase functions

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,33 +1,33 @@
-// import * as firebase from "firebase-admin";
-//
-// // connect to firebase
-// // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-// const firebaseCredentials: any = {
-//     type: process.env.F_FIREBASE_TYPE,
-//     project_id: process.env.F_FIREBASE_PROJECT_ID,
-//     private_key_id: process.env.F_FIREBASE_PRIVATE_KEY_ID,
-//     private_key: process.env.F_FIREBASE_PRIVATE_KEY
-//         ? process.env.F_FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
-//         : undefined,
-//     client_email: process.env.F_FIREBASE_CLIENT_EMAIL,
-//     client_id: process.env.F_FIREBASE_CLIENT_ID,
-//     auth_uri: process.env.F_FIREBASE_AUTH_URI,
-//     token_uri: process.env.F_FIREBASE_TOKEN_URI,
-//     auth_provider_x509_cert_url: process.env.F_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-//     client_x509_cert_url: process.env.F_FIREBASE_CLIENT_X509_CERT_URL,
-// }
-//
-// for (const [key, value] of Object.entries(firebaseCredentials)) {
-//     if (value === undefined) {
-//         console.error(`${key} environment variable is undefined `)
-//         process.exit(1);
-//     }
-// }
-//
-// // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-// firebase.initializeApp({
-//     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
-//     credential: firebase.credential.cert(firebaseCredentials),
-// })
-//
-// export const db = firebase.firestore();
+import * as firebase from "firebase-admin";
+
+// connect to firebase
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const firebaseCredentials: any = {
+    type: process.env.F_FIREBASE_TYPE,
+    project_id: process.env.F_FIREBASE_PROJECT_ID,
+    private_key_id: process.env.F_FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.F_FIREBASE_PRIVATE_KEY
+        ? process.env.F_FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+        : undefined,
+    client_email: process.env.F_FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.F_FIREBASE_CLIENT_ID,
+    auth_uri: process.env.F_FIREBASE_AUTH_URI,
+    token_uri: process.env.F_FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.F_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.F_FIREBASE_CLIENT_X509_CERT_URL,
+}
+
+for (const [key, value] of Object.entries(firebaseCredentials)) {
+    if (value === undefined) {
+        console.error(`${key} environment variable is undefined `)
+        process.exit(1);
+    }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+firebase.initializeApp({
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
+    credential: firebase.credential.cert(firebaseCredentials),
+})
+
+export const db = firebase.firestore();
--- functions/src/config/twilio.ts
+++ functions/src/config/twilio.ts
@@ -1,21 +1,21 @@
-// import twilio from "twilio";
-//
-// const accountSid = (process.env.TWILIO_ACCOUNT_SID || '').toString();
-// const authToken = (process.env.TWILIO_AUTH_TOKEN || '').toString();
-// export const twilioPhoneNumber: string = (process.env.TWILIO_PHONE_NUMBER || '').toString();
-// export const twilioClient: twilio.Twilio = twilio(accountSid, authToken);
-// // url to handle questions
-// export const voiceEndpoint = 'handle-phone-survey-input';
-// export const voiceURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${voiceEndpoint}`;
-// // url to update status of phone calls
-// export const eventEndpoint = 'handle-phone-survey-event-update';
-// export const eventURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${eventEndpoint}`;
-// // url to update status of phone calls
-// export const failureEndpoint = 'handle-phone-survey-failed';
-// export const failureURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${failureEndpoint}`;
-// // url to handle recording updates
-// export const recordingEndpoint = 'recording-events';
-// export const recordingURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${recordingEndpoint}`;
-// // url to handle transcription updates
-// export const transcriptionEndpoint = 'transcription-events';
-// export const transcriptionURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${transcriptionEndpoint}`;
+import * as twilio from "twilio";
+
+const accountSid = (process.env.TWILIO_ACCOUNT_SID || '').toString();
+const authToken = (process.env.TWILIO_AUTH_TOKEN || '').toString();
+export const twilioPhoneNumber: string = (process.env.TWILIO_PHONE_NUMBER || '').toString();
+export const twilioClient: twilio.Twilio = twilio(accountSid, authToken);
+// url to handle questions
+export const voiceEndpoint = 'handle-phone-survey-input';
+export const voiceURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${voiceEndpoint}`;
+// url to update status of phone calls
+export const eventEndpoint = 'handle-phone-survey-event-update';
+export const eventURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${eventEndpoint}`;
+// url to update status of phone calls
+export const failureEndpoint = 'handle-phone-survey-failed';
+export const failureURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${failureEndpoint}`;
+// url to handle recording updates
+export const recordingEndpoint = 'recording-events';
+export const recordingURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${recordingEndpoint}`;
+// url to handle transcription updates
+export const transcriptionEndpoint = 'transcription-events';
+export const transcriptionURL = `${process?.env?.TWILIO_STATUS_CALLBACK_URL || ''}/${transcriptionEndpoint}`;
