452b198d52e224e8b385aeb374450814566f5507
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-05 19:50:37 -0500

    Page for Users to Submit Support Tickets that Admin can View (#49)
    Create a page for users to submit support tickets that admin account can
    view.
    
    ---------
    
    Co-authored-by: Richard Luo <luo.richard@gmail.com>
    Co-authored-by: Tanishq Jain <tjain44@gatech.edu>
    Co-authored-by: Vivek Vijaykumar <vvijaykumar6@gatech.edu>

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,7 @@ import AdminFacilities from './adminfacilitydashboard';
 import AdminPhoneSurveyResponsesPage from './adminphonesurveyresponses';
 import AdminPhoneSurveyQueuePage from './adminphonesurveyqueue';
 import CreateAdminPage from './createadmin';
+import SupportTicketPage from './supportticket';
 
 // Define routes
 /* eslint-disable @typescript-eslint/naming-convention */
@@ -35,6 +36,7 @@ export const k_admin_facility_page_route = '/admin-facility-dashboard';
 export const k_admin_phone_survey_responses_page_route = '/admin-phone-survey-responses';
 export const k_admin_phone_survey_queue_page_route = '/admin-phone-survey-queue';
 export const k_create_admin_page_route = '/create-admin';
+export const k_support_ticket_route = '/support-ticket';
 
 const router = createBrowserRouter([
     {
@@ -100,6 +102,10 @@ const router = createBrowserRouter([
     {
         path: k_create_admin_page_route,
         element: <CreateAdminPage />,
+    },
+    {
+        path: k_support_ticket_route,
+        element: <SupportTicketPage />,
     },
 ]);
 
