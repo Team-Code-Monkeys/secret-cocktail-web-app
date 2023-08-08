e4775fb6868bfc27281541aba47b8b472540432f
ATheSecretCocktail <84815195+ATheSecretCocktail@users.noreply.github.com>
2023-08-08 09:06:50 -0400

    Delete api.ts
    Delete exposed credentials.

--- src/api.ts
+++
@@ -1,3 +0,0 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? 'https://us-central1-secret-cocktail.cloudfunctions.net/app' : 'http://127.0.0.1:5001/secret-cocktail/us-central1/app';
-export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || 'AIzaSyAxAi1EsechS9MWXOHhY53u2JthptfPN1g';
-export const GOOGLE_GEOCODING_API_KEY = process.env.REACT_APP_GOOGLE_GEOCODING_API_KEY || 'AIzaSyAxAi1EsechS9MWXOHhY53u2JthptfPN1g';
