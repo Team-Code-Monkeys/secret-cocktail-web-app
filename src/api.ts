7d7986cc621a7437f93137c43964b489c77d0bbb
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-08-09 09:01:25 -0700

    Use new API key for maps and geodecoding

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' ? 'https://us-central1-tsc-phone-survey.cloudfunctions.net/app' : 'http://127.0.0.1:5001/tsc-phone-survey/us-central1/app';
-export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || 'AIzaSyATOLjzu4dudN72tlNG5-5NgNSqjWWNuxA';
-export const GOOGLE_GEOCODING_API_KEY = process.env.REACT_APP_GOOGLE_GEOCODING_API_KEY || 'AIzaSyATOLjzu4dudN72tlNG5-5NgNSqjWWNuxA';
+export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || 'AIzaSyA7yLol93RZlUbfHfnD1_-rw4T1m47YV10';
+export const GOOGLE_GEOCODING_API_KEY = process.env.REACT_APP_GOOGLE_GEOCODING_API_KEY || 'AIzaSyA7yLol93RZlUbfHfnD1_-rw4T1m47YV10';
